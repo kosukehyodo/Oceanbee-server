1aaca97aee88cf7cf29c7400ba373132b9cc1ab1
兵頭洸介 <kosukehyodo0216@gmail.com>
2020-06-09 16:36:55 +0900

    add column to users table for token and user status

---
+++ database/migrations/2020_06_09_073039_add_email_verify_token_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddEmailVerifyTokenToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('email_verify_token')->nullable()->after('email');
+            $table->tinyInteger('status')->after('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('email_verify_token');
+            $table->dropColumn('status');
+        });
+    }
+}
