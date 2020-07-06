28b8257b28f0e4132ff024fa6fbd63aaf870461f
兵頭洸介 <kosukehyodo0216@gmail.com>
2020-07-07 07:51:33 +0900

    show table layout

--- resources/views/project/plan/create.blade.php
+++ resources/views/project/plan/create.blade.php
@@ -47,9 +47,186 @@
             </div>
             <div class="p-plan-create__calendar">
                 <h4>日付を登録</h4>
-                <p>プランを提供できる時間帯を設定してください（単発プランの場合は必須）
-                    時間帯を伴なわないプラン（月額で一ヶ月レンタルなど）は設定不要で、開始日をユーザーとチャットでやりとりして決めてください</p>
-                <input type="text" id="flatpickr">
+                <p>プランを提供できる時間帯を設定してください（単発プランの場合は必須）</p>
+                <p>時間帯を伴なわないプラン（月額で一ヶ月レンタルなど）は設定不要で、開始日をユーザーとチャットでやりとりして決めてください</p>
+                <!-- <input type="text" id="flatpickr"> -->
+                <table class="innerTable nowrap jscInnerTable" cellpadding="0" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th rowspan="2" class="weekPaging"><span class="arrowPagingWeekLOff">前の一週間</span></th>
+                            <th colspan="14" class="monthCell">2020年7月</th>
+                            <th rowspan="2" class="weekPaging"><a title="次の一週間"
+                                    class="arrowPagingWeekR jscCalNavi">次の一週間</a></th>
+                        </tr>
+                        <tr class="dayCellContainer">
+                            @for($i = now(); $i < now()->addDay(13); $i->addDay())
+                                <th class="dayCell">{{$i->day}} <br />{{$i->isoFormat('ddd')}}</th>
+                                @endfor
+                        </tr>
+                    </thead>
+                    <tr>
+                        <th>
+                            <table>
+                                <tr>
+                                    <th class="timeCell timeSharpLine">08：00</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell timeSharpLine">08：30</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell timeSharpLine">09：00</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell timeSharpLine">09：30</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell timeSharpLine">10：00</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell ">10：30</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell timeSharpLine">11：00</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell ">11：30</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell timeSharpLine">12：00</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell ">12：30</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell timeSharpLine">13：00</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell ">13：30</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell timeSharpLine">14：00</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell ">14：30</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell timeSharpLine">15：00</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell ">15：30</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell timeSharpLine">16：00</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell ">16：30</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell timeSharpLine">17：00</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell ">17：30</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell timeSharpLine">18：00</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell ">18：30</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell timeSharpLine">19：00</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell timeSharpLine">19：30</th>
+                                </tr>
+                                <tr>
+                                    <th class="timeCell timeSharpLine">20：00</th>
+                                </tr>
+                            </table>
+                        </th>
+                        @for($i = now(); $i < now()->addDay(13); $i->addDay())
+                            <th class="innerCol">
+                                <table cellpadding="0" cellspacing="0" class="moreInnerTable">
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 08:00')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 08:30')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 09:00')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 09:30')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 10:00')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 10:30')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 11:00')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 11:30')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 12:00')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 12:30')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 13:00')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 13:30')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 14:00')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 14:30')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 15:00')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 15:30')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 16:00')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 16:30')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 17:00')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 17:30')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 18:00')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 18:30')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 19:00')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 19:30')}}"></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="closeCell"><input name="agree" type="checkbox" value="{{$i->format('Y-m-d 20:00')}}"></td>
+                                    </tr>
+                                </table>
+                            </th>
+                            @endfor
+                    </tr>
+                </table>
             </div>
 
         </div>
